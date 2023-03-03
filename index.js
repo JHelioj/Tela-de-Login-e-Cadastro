af7476a3698e108c829acfd1bfb05d4df8343025
JHelioj <jhelionetodev@gmail.com>
2023-03-03 11:15:20 -0300

    Criada a função de mudança de botões

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-var formLogin = document.querySelector('#login')
+var formLogin = document.querySelector('#login');
+var formCadastro = document.querySelector('#cadastro');
+var btnColor = document.querySelector('.btnColor');
+
+document.querySelector('#btnLogin').addEventListener('click', () => {
+    formLogin.style.left = "25px"
+    formCadastro.style.left = "450px"
+    btnColor.style.left = "0px"
+});
+
+document.querySelector('#btnCadastre ').addEventListener('click', () => {
+    formLogin.style.left = "-450px"
+    formCadastro.style.left = "25px"
+    btnColor.style.left = "110px"
+});
