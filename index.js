7c0507ea4679080ea86fa84298e45bd9ebec07cb
JHelioj <jhelionetodev@gmail.com>
2023-03-09 09:26:38 -0300

    Validação de login criada e redirecionamento para página home

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-var formLogin = document.querySelector('#login');
-var formCadastro = document.querySelector('#cadastro');
-var btnColor = document.querySelector('.btnColor');
+//Animação de ir para o lado (Botões: Login e Cadastro)
 
-document.querySelector('#btnLogin').addEventListener('click', () => {
-    formLogin.style.left = "25px"
-    formCadastro.style.left = "450px"
-    btnColor.style.left = "0px"
+var formLogin = document.querySelector("#formlogin");
+var formCadastro = document.querySelector("#formcadastro");
+var btnColor = document.querySelector(".btnColor");
+
+document.querySelector("#btnLogin").addEventListener("click", () => {
+  formLogin.style.left = "25px";
+  formCadastro.style.left = "450px";
+  btnColor.style.left = "0px";
 });
 
-document.querySelector('#btnCadastre').addEventListener('click', () => {
-    formLogin.style.left = "-450px"
-    formCadastro.style.left = "25px"
-    btnColor.style.left = "110px"
+document.querySelector("#btnCadastre").addEventListener("click", () => {
+  formLogin.style.left = "-450px";
+  formCadastro.style.left = "25px";
+  btnColor.style.left = "110px";
 });
+
+// Validação Login
+
+function logar() {
+  var login = document.getElementById("login").value;
+  var senha = document.getElementById("senha").value;
+
+  if (login == "admin" && senha == "admin") {
+        location.href = "home.html";
+  } else {
+    alert("Usuário ou senha incorretos");
+  }
+}
